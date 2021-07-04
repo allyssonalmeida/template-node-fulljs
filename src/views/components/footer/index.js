cdd5fd7c402483b34a69d3b9034c5748aa093a46
allyssonalmeida <allyssonif@gmail.com>
2021-07-04 10:32:15 -0300

    Nomeando as Classes

--- src/views/components/footer/index.js
+++ src/views/components/footer/index.js
@@ -1,6 +1,6 @@
 import planet from '../../../assets/images/elements/planet.png';
 let footer = `
-<footer>
+<footer class="container footer-contain">
   <p>Star wars  -  este é um exemplo criado para os Alunos Gama Academy
   Módulo de boas práticas.</p>
   <img src="${planet}" height="180px" width="auto" alt="Death Star">
