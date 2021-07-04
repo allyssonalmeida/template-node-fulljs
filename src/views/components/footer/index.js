5aa2fd0fd284362114e997c74a2b423e066a11c9
allyssonalmeida <allyssonif@gmail.com>
2021-07-03 22:12:51 -0300

    Estruturando Elementos

---
+++ src/views/components/footer/index.js
@@ -0,0 +1,9 @@
+import planet from '../../../assets/images/elements/planet.png';
+let footer = `
+<footer>
+  <p>Star wars  -  este é um exemplo criado para os Alunos Gama Academy
+  Módulo de boas práticas.</p>
+  <img src="${planet}" height="180px" width="auto" alt="Death Star">
+</footer>`;
+
+export default footer;
